d93aa74b50cd0d2e4d46ec76c661a401ad092582
Etterke <etterke@gmail.com>
2023-11-08 23:12:04 +0100

    added basic components and routing

--- iata-sample-app/src/app/app.routes.ts
+++ iata-sample-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { NavigationComponent } from './presentation/navigation/navigation.component';
+import { SidebarComponent } from './presentation/sidebar/sidebar.component';
+import { ContentComponent } from './presentation/content/content.component';
+import { DashboardComponent } from './container/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'navigation', component: NavigationComponent},
+    {path: 'sidebar', component: SidebarComponent},
+    {path: 'content', component: ContentComponent}
+];
